c3c658019de9b3ceaf64b3073b4112fa4cb348d9
verdurevigor <brody.91@outlook.com>
2016-02-05 18:36:17 -0800

    Some notes added

--- EugeneCommunity/EugeneCommunity/Models/TopicViewModel.cs
+++ EugeneCommunity/EugeneCommunity/Models/TopicViewModel.cs
@@ -8,10 +8,12 @@ namespace EugeneCommunity.Models
     public class TopicViewModel
     {
         List<Message> posts = new List<Message>();
-        // Using virtual keyword is NOT necessary with ViewModels! Ok in domain model though.
-        // Adding virtual means the propety isn't instantiated until it is set.
+        // Using virtual keyword is NOT necessary (or practical) with ViewModels! Ok in domain model though.
+        // Adding virtual means the propety isn't instantiated/existent until it is set.
         public int TopicId { get; set; }
         public string Name { get; set; }
+        // This property should be set by querying the posts for the most recent dated message
+        public string LastPost { get; set; }
 
         public List<Message> Posts
         {
